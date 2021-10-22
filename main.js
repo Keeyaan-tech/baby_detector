e163087cfacf2f2f9d87543f258ea6ed22d8b1b6
Keeyaan-tech <78153976+Keeyaan-tech@users.noreply.github.com>
2021-10-22 19:53:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-objectDetector = "";
-img = "";
+song = "";
 objects = [];
 status = "";
 
 function preload() {
     img = loadImage('dog_cat.jpg');
+    song = loadSound("monster_mash.mp3");
 }
 
 
@@ -12,6 +12,7 @@ function setup() {
     canvas = createCanvas(380, 380);
     canvas.center();
     video = createCapture(VIDEO);
+    video.size(380, 380);
     video.hide();
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
@@ -30,7 +31,6 @@ function gotResult(error, results) {
     objects = results;
 }
 
-
 function draw() {
     image(video, 0, 0, 380, 380);
 
@@ -38,19 +38,31 @@ function draw() {
         objectDetector.detect(video, gotResult);
         for (var i = 0; i < objects.length; i++) {
             document.getElementById("status").innerHTML = "Status : Object Detected";
-            if (objects.length >= 1) {
-                document.getElementById("baby_status").innerHTML = "Baby Found";
-            } else {
-                document.getElementById("baby_status").innerHTML = "Baby Not Found";
-            }
 
-            
             fill(255, 0, 0);
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + " " + percent + "%", objects[i].x + 30, objects[i].y + 30);
             noFill();
             stroke(255, 0, 0);
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if (objects[i].label == "person") {
+                document.getElementById("number_of_objects").innerHTML = "Baby Found";
+                console.log("stop");
+                song.stop();
+            } else {
+                document.getElementById("number_of_objects").innerHTML = "Baby Not Found";
+                console.log("play");
+                song.play();
+            }
+        }
+        if (objects.length == 0) {
+            document.getElementById("number_of_objects").innerHTML = "Baby Not Found";
+            console.log("play");
+            song.play();
+
+
+
         }
     }
 }
